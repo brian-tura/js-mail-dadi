96f06aba33094d18412257241526eaecbb4464e4
brian-tura <b.tura2002@gmail.com>
2025-03-21 08:41:45 +0100

    Mail excercise done, forgot to push

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Esercizio Mail
+
+const mail = ["ginopippo@gmail.com", "nonhocreativita@gmail.com", "sonostancocapogmail.com"];
+let mailUtente = prompt("Inserisci la tua mail");
+let mailCheck = false
+
+for(let i = 0; i<mail.length; i++) {
+    if(mailUtente === mail[i]){
+        mailCheck = true;
+    }
+}
+
+if(mailCheck == true){
+    console.log("Accesso consentito");
+}
+else{
+    console.log("Accesso negato");
+}
